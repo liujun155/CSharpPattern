4510812b1c8a98dbf599c362c941477e0e0f1032
liujun <354277337@qq.com>
2023-02-28 17:18:39 +0800

    建造者模式完善

--- CSharp设计模式/Program.cs
+++ CSharp设计模式/Program.cs
@@ -32,20 +32,7 @@ namespace CSharp设计模式
 
             #region 建造者模式
             Console.WriteLine("\n===============建造者模式===============");
-            // 客户找到电脑城老板说要买电脑，这里要装两台电脑
-            // 创建指挥者和构造者
-            Director director = new Director();
-            Builder b1 = new ConcreteBuilder1();
-            Builder b2 = new ConcreteBuilder2();
-            // 老板叫员工去组装第一台电脑
-            director.Construct(b1);
-            // 组装完，组装人员搬来组装好的电脑
-            Computer computer1 = b1.GetComputer();
-            computer1.Show();
-            // 老板叫员工去组装第二台电脑
-            director.Construct(b2);
-            Computer computer2 = b2.GetComputer();
-            computer2.Show();
+            BuilderExample.Run();
             #endregion
 
             #region 原型模式
